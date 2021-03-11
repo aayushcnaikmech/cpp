b7b2001d41e007dcd555abd4a65946b672c004be
aayushcnaikmech <78956541+aayushcnaikmech@users.noreply.github.com>
2021-03-11 12:52:05 +0530

    Update roots of a quadratic equation.c

--- roots of a quadratic equation.c
+++ roots of a quadratic equation.c
@@ -2,29 +2,33 @@
 #include<math.h>
 int main()
 {
-     int a,b,c,discriminant,root1,root2,rpart,ipart;
-     printf("Enter a,b and c:");
-     scanf("%d,%d,%d",&a,&b,&c);
+   double a, b, c, discriminant, root1, root2, realPart, imaginaryPart;
 
-     discriminant=b*b-4*a*c;
+printf("Enter coefficients a, b and c: ");
+scanf(“%lf %lf %lf”,&a, &b, &c);
 
-     if(discriminant>0)
+discriminant = b*b-4*a*c;
+
+if (discriminant > 0)
 {
-     root1 = (-b + sqrt(discriminant))/(2*a);
-     root2 = (-b - sqrt(discriminant))/(2*a);
-     printf("root1=%.2d and root2=%.2d",root1,root2);
+root1 = (-b+sqrt(discriminant))/(2*a);
+root2 = (-b-sqrt(discriminant))/(2*a);
+
+printf("root1 = %.2lf and root2 = %.2lf",root1 , root2);
 }
-     else if(discriminant==0)
+
+else if (discriminant == 0)
 {
-     root1 = -b/(2*a);
-     root2 = -b/(2*a);
-     printf("root1=%.2d and root2=%.2d",root1,root2);
+root1 = root2 = -b/(2*a);
+
+printf(“root1 = root2 = %.2lf;”, root1);
 }
-     else
+else
 {
-     rpart=-b/(2*a);
-     ipart=sqrt(-discriminant)/(2*a);
-     printf("root1 = %.2d+%.2di and root2 = %.2d+%.2di",rpart,ipart,rpart,ipart);
+realPart = -b/(2*a);
+imaginaryPart = sqrt(-discriminant)/(2*a);
+printf("root1 = %.2lf+%.2lfi and root2 = %.2f-%.2fi", realPart, imaginaryPart, realPart, imaginaryPart);
 }
+
 return 0;
-}
+} 
